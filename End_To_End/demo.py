1d529b01f2fd10e81fec3b9d1ea1e6a537a1b85f
Prabha <prabha.m2022ai-ds@sece.ac.in>
2024-06-22 21:31:04 +0530

    added

---
+++ End_To_End/demo.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+
+data = pd.read_csv(r'C:\Users\ADMIN\Documents\Projects\ML_projects\END_TO_END\End_To_End_ML\End_To_End\SpotifySongPopularityAPIExtract.csv')
+
+print(data)
---
+++ End_To_End/main.py
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+from src.Music_Popularity_Prediction_End_to_End_ML_Project.config.configuration import main
+=======
+from src.music_popularity_prediction.config.configuration import main
+>>>>>>> 6451e23 (requirements added)
